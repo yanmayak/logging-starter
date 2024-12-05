a60207b0f115a3582766f03dc87ad7ee956b3e5b
Marina Dvoretskova <smallestproblem@gmail.com>
2024-12-05 15:03:41 +0300

    init

---
+++ src/main/java/ru/yanmayak/aspect/LoggingAspect.java
@@ -0,0 +1,75 @@
+package ru.yanmayak.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Aspect
+@Slf4j
+public class LoggingAspect {
+    private final LoggingConfig loggingConfig;
+
+    public LoggingAspect(LoggingConfig loggingConfig) {
+        this.loggingConfig = loggingConfig;
+    }
+
+    private boolean shouldLog(LoggingLevel log) {
+        return log.suitable(this.loggingConfig.getLevel());
+    }
+
+    @Before("@annotation(LogExecution)")
+    public void logBefore(JoinPoint joinPoint) {
+        if(shouldLog(LoggingLevel.INFO)) {
+            log.info("Вызван метод {}", joinPoint.getSignature().getName());
+        }
+    }
+
+    @After("@annotation(LogExecution)")
+    public void logAfter(JoinPoint joinPoint) {
+        if(shouldLog(LoggingLevel.INFO)) {
+            log.info("Метод {} завершен", joinPoint.getSignature().getName());
+        }
+    }
+
+    @AfterThrowing(pointcut = "@annotation(LogException)", throwing = "throwable")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable throwable) {
+        if(shouldLog(LoggingLevel.WARN)) {
+            log.warn("Возникло исключение {} : {} в методе: {}",
+                    throwable.getClass().getName(),
+                    throwable.getMessage(),
+                    joinPoint.getSignature().getName()
+            );
+        }
+    }
+
+    @AfterReturning(value = "@annotation(LogExecution)", returning = "returnValue")
+    public void logAfterReturning(JoinPoint joinPoint, Object returnValue) {
+        if(shouldLog(LoggingLevel.INFO)) {
+            log.info("Метод {} вернул {}", joinPoint.getSignature().getName(), returnValue);
+        }
+    }
+
+    @Around("@annotation(LogAroundVoid)")
+    public void logAroundVoidMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (shouldLog(LoggingLevel.INFO)) {
+            log.info("Метод {} вызван", joinPoint.getSignature().getName());
+        }
+        try {
+            joinPoint.proceed();
+            if (shouldLog(LoggingLevel.INFO)) {
+                log.info("Метод {} выполнен успешно", joinPoint.getSignature().getName());
+            }
+        }
+        catch (Throwable throwable) {
+            if (shouldLog(LoggingLevel.ERROR)) {
+                log.error("Метод {} завершен с ошибкой: {} ",
+                        joinPoint.getSignature().getName(),
+                        throwable.getMessage()
+                );
+            }
+            throw throwable; //throwable выбран в связи с наличием ControllerAdvice,
+            // который обработает строго определенное исключение6 прописанное в сервисе
+        }
+    }
+}
---
+++ src/main/java/ru/yanmayak/aspect/LoggingLevel.java
@@ -0,0 +1,23 @@
+package ru.yanmayak.aspect;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LoggingLevel {
+    ALL(7),
+    TRACE(6),
+    DEBUG(5),
+    INFO(4),
+    WARN(3),
+    ERROR(2),
+    FATAL(1),
+    OFF(0);
+
+    private final int level;
+
+    boolean suitable(LoggingLevel loggingLevel) {
+        return this.level <= loggingLevel.level;
+    }
+}
